804da18b0ec91be49f509b8b8b24d7ac26526862
shivaniwaghmare9 <shivaniw69@gmail.com>
2024-12-14 17:54:49 +0530

    firt commit

--- new.cpp
+++ new.cpp
@@ -1,2 +1,93 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
-using 
+using namespace std;
+int main() {
+   for(int r=1; r<=12; ++r)
+   {
+   for(int c=1; c<=12; ++c)
+       {
+   if(c<=r)
+           {
+              cout<<"*"; 
+              
+           }
+  else
+           {
+               cout<<" ";
+           }
+       }
+   
+   
+   cout<<endl;
+   }
+}
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+   for(int r=1; r<=5; ++r)
+   {
+   for(int c=1; c<=5; ++c)
+       {
+   if(c<=6-r)
+           {
+              cout<<"*"; 
+              
+           }
+  else
+           {
+               cout<<" ";
+           }
+       }
+   
+   
+   cout<<endl;
+   }
+}
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+   for(int r=1; r<=5; ++r)
+   {
+   for(int c=1; c<=5; ++c)
+       {
+   if(c>=r)
+           {
+              cout<<"*"; 
+              
+           }
+  else
+           {
+               cout<<" ";
+           }
+       }
+   
+   
+   cout<<endl;
+   }
+}
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+   for(int r=1; r<=5; ++r)
+   {
+   for(int c=1; c<=5; ++c)
+       {
+   if(c>=6-r)
+           {
+              cout<<"*"; 
+              
+           }
+  else
+           {
+               cout<<" ";
+           }
+       }
+   
+   
+   cout<<endl;
+   }
+}
+
