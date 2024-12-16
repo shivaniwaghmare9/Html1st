93fb6390e71d15d3f55e9e4b8d13e74987271f3f
shivaniwaghmare9 <shivaniw69@gmail.com>
2024-12-16 17:01:42 +0530

    updation

--- new.cpp
+++ new.cpp
@@ -2,92 +2,24 @@
 #include <iostream>
 using namespace std;
 int main() {
-   for(int r=1; r<=12; ++r)
-   {
-   for(int c=1; c<=12; ++c)
-       {
-   if(c<=r)
-           {
-              cout<<"*"; 
-              
-           }
-  else
-           {
-               cout<<" ";
-           }
-       }
-   
-   
-   cout<<endl;
-   }
+    for(int i=1;i<=5; ++i)
+    {
+        for(int j=1; j<=i; ++j)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
 }
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 int main() {
-   for(int r=1; r<=5; ++r)
-   {
-   for(int c=1; c<=5; ++c)
-       {
-   if(c<=6-r)
-           {
-              cout<<"*"; 
-              
-           }
-  else
-           {
-               cout<<" ";
-           }
-       }
-   
-   
-   cout<<endl;
-   }
+    for(int i=1;i<=5; ++i)
+    {
+        for(int j=1; j<=i; ++j)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
 }
-// Online C++ compiler to run C++ program online
-#include <iostream>
-using namespace std;
-int main() {
-   for(int r=1; r<=5; ++r)
-   {
-   for(int c=1; c<=5; ++c)
-       {
-   if(c>=r)
-           {
-              cout<<"*"; 
-              
-           }
-  else
-           {
-               cout<<" ";
-           }
-       }
-   
-   
-   cout<<endl;
-   }
-}
-// Online C++ compiler to run C++ program online
-#include <iostream>
-using namespace std;
-int main() {
-   for(int r=1; r<=5; ++r)
-   {
-   for(int c=1; c<=5; ++c)
-       {
-   if(c>=6-r)
-           {
-              cout<<"*"; 
-              
-           }
-  else
-           {
-               cout<<" ";
-           }
-       }
-   
-   
-   cout<<endl;
-   }
-}
-
