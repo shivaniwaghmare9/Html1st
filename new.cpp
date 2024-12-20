807822c2ece0641d0bf4cbb0ecad88834fbe7e23
shivaniwaghmare9 <shivaniw69@gmail.com>
2024-12-20 17:35:48 +0530

    unique

--- new.cpp
+++ new.cpp
@@ -1,5 +1,5 @@
 // Online C++ compiler to run C++ program online
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 int main() {
     for(int i=1;i<=5; ++i)
@@ -22,4 +22,26 @@ int main() {
         }
         cout<<endl;
     }
+}*/
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[]={1,2,1,3,2,1};
+ for(int i=0; i<6; ++i) 
+ {
+     bool dup=false;
+     for(int j=i+1; j<6; ++j)
+     {
+         if(arr[i]==arr[j] && arr[i]!=-1)
+         {
+             arr[j]=-1;
+             dup=true;
+         }
+     }
+     if(!dup  && arr[i]!=-1)
+     {
+         cout<<arr[i];
+     }
+ }
 }
